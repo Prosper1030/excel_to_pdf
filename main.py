eaf87407d3f57aa3269d8bb71afb875a39000300
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-20 23:25:33 +0800

    Add header total price and company name

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
+
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
@@ -21,23 +22,41 @@ for filepath in filepaths:
 
     # Add headers
     columns = list(df.columns)
+    column_dict = columns
     columns = [item.replace("_", " ").title() for item in columns]
     pdf.set_font(family="Times", size=10, style="B")
     pdf.set_text_color(80, 80, 80)
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=60, h=8, txt=columns[1], border=1)
-    pdf.cell(w=40, h=8, txt=columns[2], border=1)
-    pdf.cell(w=30, h=8, txt=columns[3], border=1)
-    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+    w_data = [30, 60, 40, 30, 30]
+
+    for i in range(5):
+        if i != 4:
+            pdf.cell(w=w_data[i], h=8, txt=columns[i], border=1, ln=0)
+        else:
+            pdf.cell(w=w_data[i], h=8, txt=columns[i], border=1, ln=1)
 
     # Add rows to table
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
-        pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+        for i in range(5):
+            if i != 4:
+                pdf.cell(w=w_data[i], h=8, txt=str(row[column_dict[i]]), border=1, ln=0)
+            else:
+                pdf.cell(w=w_data[i], h=8, txt=str(row[column_dict[i]]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    for i in range(5):
+        if i != 4:
+            pdf.cell(w=w_data[i], h=8, txt="", border=1, ln=0)
+        else:
+            pdf.cell(w=w_data[i], h=8, txt=str(total_sum), border=1, ln=1)
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+    # Add company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
